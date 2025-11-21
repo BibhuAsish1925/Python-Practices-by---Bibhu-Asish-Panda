3393442e8bdf2c74daddf68bebe825e5f4f1d541
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-11-21 22:42:15 +0530

    Add files via upload

---
+++ Day-01/Datatypes.py
@@ -0,0 +1,89 @@
+#----------------Basics--------------
+
+# int (integer)
+a = 10
+temp = -5
+print("a =", a)
+print("temp =", temp)
+
+print()
+
+# float
+voltage = 1.98
+resistance = 47.5
+print("voltage =", voltage)
+print("resistance =", resistance)
+
+print()
+
+# str (string)
+name = "Bibhu"
+msg = "Hello World"
+print("name =", name)
+print("msg =", msg)
+
+print()
+
+# bool (Boolean)
+flag = True
+error = False
+print("flag =", flag)
+print("error =", error)
+
+print()
+
+# list
+values = [10, 20, 30]
+print("values =", values)
+
+print()
+
+# tuple (Ordered, unchangeable)
+coordinates = (10, 20)
+print("coordinates =", coordinates)
+
+print()
+
+# set
+unique_ids = {1, 2, 3}
+print("unique_ids =", unique_ids)
+
+print()
+
+# dict (dictionary)
+timing = {
+    "path": "U1/U2",
+    "slack": -0.23
+}
+print("timing =", timing)
+print("timing['path'] =", timing["path"])
+print("timing['slack'] =", timing["slack"])
+
+print()
+print()
+
+#----------------Very Important--------------
+
+# Check the Type Using type()
+a = 10
+print("Type of a:", type(a))
+
+print()
+
+# Type Conversion (Casting)
+x = "10"
+y = int(x)
+print("x (string) =", x)
+print("y (converted to int) =", y)
+
+print()
+
+a = 10
+b = float(a)
+print("a =", a)
+print("b (float) =", b)
+
+print()
+
+c = str(100)
+print("c (string) =", c)
---
+++ Day-01/Variables.py
@@ -0,0 +1,24 @@
+x = 10            #integer
+name = "Bibhu"    #text
+voltage = 1.8     #float
+
+print(x, name, voltage)
+
+print()
+
+# rules of writing variables
+ 
+#valid
+count = 5
+file_name = "report.txt"
+_voltage = 1.8
+print(count,file_name, _voltage)
+
+print()
+
+#invalid
+"""2value = 10
+file-name = "log"
+class = 5   # reserved keyword
+
+print(2value, file-name, class)"""
